68ba263db961f929426de87ac444dee0a2728b8f
stabur <burnov@ya.ru>
2022-09-30 09:39:32 +0300

    30.09.2022_9:40

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QFileDialog
 import MySQLdb
 import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import os.path
 import pandas as pd
 import openpyxl
 from sshtunnel import SSHTunnelForwarder
 from transliterate import translit, get_available_language_codes
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 Form, Window = uic.loadUiType("forma1.ui")
 
@@ -17,31 +20,11 @@ form = Form()
 form.setupUi(window)
 window.show()
 
-ssh_host = []
-ssh_port = []
+server = []
+dbconnect = []
 
-if (ssh_host and ssh_port):
-    print(ssh_host, ssh_port)
-else:
-    print("Переменные ssh-host и ssh-порт - пусты!")
-
-server = SSHTunnelForwarder(
-    ('192.168.1.35', 22),
-    ssh_username='burnov',
-    ssh_password='01021989',
-    remote_bind_address=('127.0.0.1', 3306)
-)
-
-if (server):
-    print(server)
-    print("Соединение c ssh-сервером установленно!")
-else:
-    print("Проблема с подключением к ssh-серверу!")
-
-server.start()
-
-def on_click_testbd():
-    global host, login, password, db_name, table_name, ssh_host, ssh_port, ssh_login, ssh_pass, ssh_mysql_host, ssh_mysql_port
+def on_click_testssh():
+    global server, ssh_host, ssh_port, ssh_login, ssh_pass, ssh_mysql_host, ssh_mysql_port
 
     ssh_host = form.lineEdit.text()
     ssh_port = form.lineEdit_2.text()
@@ -49,13 +32,6 @@ def on_click_testbd():
     ssh_pass = form.lineEdit_4.text()
     ssh_mysql_host = form.lineEdit_5.text()
     ssh_mysql_port = form.lineEdit_6.text()
-
-    host = form.lineEdit_7.text()
-    login = form.lineEdit_8.text()
-    password = form.lineEdit_9.text()
-    db_name = form.lineEdit_10.text()
-
-    port = server.local_bind_port
 
     if not ssh_host:
         form.label_7.setText("<font color=red>Не заполнено поле Адрес SSH сервера!</font>")
@@ -69,32 +45,61 @@ def on_click_testbd():
         form.label_7.setText("<font color=red>Не заполнено поле Адрес MySql сервера!</font>")
     elif not ssh_mysql_port:
         form.label_7.setText("<font color=red>Не заполнено поле Порт Mysql сервера!</font>")
-    elif not host:
-        form.label_7.setText("<font color=red>Не заполнено поле Хост!</font>")
-    elif not login:
-        form.label_7.setText("<font color=red>Не заполнено поле Логин!</font>")
-    elif not password:
-        form.label_7.setText("<font color=red>Не заполнено поле Пароль!</font>")
-    elif not db_name:
-        form.label_7.setText("<font color=red>Не заполнено поле База Данных!</font>")
 
     if (ssh_host and ssh_port and ssh_login and ssh_pass and ssh_mysql_host and ssh_mysql_port):
-        print(ssh_host)
-    else:
-        print("Переменная ssh-host - пуста!")
-
-    if (host and login and password and db_name):
         try:
-            dbconnect = MySQLdb.connect(host, login, password, db_name, port)
-            form.label_7.setText("<font color=green>Соединение установленно!</font>")
-            dbconnect.commit()
-            dbconnect.rollback()
-            dbconnect.close()
-        except:
-            form.label_7.setText("<font color=red>Соединение НЕ установленно!</font>")
+            server = SSHTunnelForwarder(
+                (ssh_host, int(ssh_port)),
+                ssh_username=ssh_login,
+                ssh_password=ssh_pass,
+                remote_bind_address=(ssh_mysql_host, int(ssh_mysql_port))
+            )
+            print(ssh_host)
+            if (server):
+                print(server)
+                print("Соединение c SSH-сервером установленно!")
+                form.label_7.setText("<font color=green>Соединение c SSH-сервером установленно!</font>")
+            else:
+                print("Проблема с подключением к SSH-серверу!")
+            server.start()
+        except Exception as e:
+            form.label_7.setText("<font color=red>Соединение c SSH-сервером НЕ установленно!</font>")
+            print(f"""Ошибка: {e}""")
+
+def on_click_testbd():
+    global server, dbconnect, mysql_host, mysql_port, mysql_login, mysql_password, mysql_db_name
+
+    if (server):
+        mysql_host = form.lineEdit_7.text()
+        mysql_login = form.lineEdit_8.text()
+        mysql_password = form.lineEdit_9.text()
+        mysql_db_name = form.lineEdit_10.text()
+        mysql_port = server.local_bind_port
+
+        if not mysql_host:
+            form.label_7.setText("<font color=red>Не заполнено поле Хост MySql!</font>")
+        elif not mysql_login:
+            form.label_7.setText("<font color=red>Не заполнено поле Логин MySql!</font>")
+        elif not mysql_password:
+            form.label_7.setText("<font color=red>Не заполнено поле Пароль MySql!</font>")
+        elif not mysql_db_name:
+            form.label_7.setText("<font color=red>Не заполнено поле База Данных MySql!</font>")
+
+        if (mysql_host and mysql_login and mysql_password and mysql_db_name):
+            try:
+                dbconnect = MySQLdb.connect(mysql_host, mysql_login, mysql_password, mysql_db_name, mysql_port)
+                form.label_7.setText("<font color=green>Соединение c MySql установленно!</font>")
+                dbconnect.commit()
+                dbconnect.rollback()
+                dbconnect.close()
+            except:
+                form.label_7.setText("<font color=red>Соединение c MySql НЕ установленно!</font>")
+    else:
+        print("Переменные 'server' или 'dbconnect' не найдены!")
+        form.label_7.setText("<font color=red>Нет подключения к SSH серверу!</font>")
 
 def on_click_excfile():
-    global host, login, password, db_name, table_name, name_col, ef2, name_col_insert
+    global mysql_table_name, mysql_name_col, mysql_ef2, mysql_name_col_insert
     excelfile = QFileDialog.getOpenFileName(None, 'OpenFile', '', 'Excel file(*.xlsx)')
     excelfilePath = excelfile[0]
     # print(excelfilePath) # полный путь к файлу на ПК
@@ -105,11 +110,11 @@ def on_click_excfile():
     exFile = exFile.replace("'", "")
     # print(exPath) # директория файла на ПК
     # print(exFile) # название файла с расширением
-    table_name = os.path.splitext(exFile)[0]
-    #print(table_name)  # название файла без расширения
+    mysql_table_name = os.path.splitext(exFile)[0]
+    #print(mysql_table_name)  # название файла без расширения
     form.label_6.setText(f"<font color=green>Файл <font color=blue>{exFile}</font> прикреплён!</font>")
     ef = pd.read_excel(excelfilePath)
-    ef2 = pd.read_excel(excelfilePath, dtype=object)
+    mysql_ef2 = pd.read_excel(excelfilePath, dtype=object)
     # tosql = ef.head()
     # tosql.index = tosql.index + 1  # Содержимое файла с разбитием на нумерацию с 1-го
     # print(tosql.index)
@@ -126,53 +131,52 @@ def on_click_excfile():
     strhead_en_arr = strhead_en.split(', ')  # Добавляем элементы в массив и разделяем их запятыми
     # print(strhead_en_arr)
     name_col = [i + ' VARCHAR(255)  not null' for i in strhead_en_arr]
-    name_col = (', '.join(map(str, name_col)))
+    mysql_name_col = (', '.join(map(str, name_col)))
 
     name_col_insert = [i for i in strhead_en_arr]
     name_col_insert = (','.join(map(str, name_col_insert)))
     name_col_insert = name_col_insert.replace(" ", "_")
     name_col_insert = name_col_insert.replace(",", ", ")
-    name_col_insert = name_col_insert.replace(".", "")
-    # print(name_col)
+    mysql_name_col_insert = name_col_insert.replace(".", "")
+    #print(name_col)
     #print(name_col_insert)
 
 def on_click_create():
-    global host, login, password, db_name, table_name, name_col, ef2
-    host = form.lineEdit.text()
-    login = form.lineEdit_2.text()
-    password = form.lineEdit_3.text()
-    db_name = form.lineEdit_4.text()
-    port = server.local_bind_port
-    if not host:
-        form.label_7.setText("<font color=red>Не заполнено поле Хост!</font>")
-    elif not login:
-        form.label_7.setText("<font color=red>Не заполнено поле Логин!</font>")
-    elif not password:
-        form.label_7.setText("<font color=red>Не заполнено поле Пароль!</font>")
-    elif not db_name:
-        form.label_7.setText("<font color=red>Не заполнено поле База Данных!</font>")
+    global server, mysql_port, dbconnect, mysql_host, mysql_login, mysql_password, mysql_db_name, mysql_table_name, mysql_name_col, mysql_name_col_insert, mysql_ef2
+    if (server and dbconnect):
+        if not mysql_host:
+            form.label_7.setText("<font color=red>Не заполнено поле Хост MySql!</font>")
+        elif not mysql_login:
+            form.label_7.setText("<font color=red>Не заполнено поле Логин MySql!</font>")
+        elif not mysql_password:
+            form.label_7.setText("<font color=red>Не заполнено поле Пароль MySql!</font>")
+        elif not mysql_db_name:
+            form.label_7.setText("<font color=red>Не заполнено поле База Данных MySql!</font>")
 
-    if (host and login and password and db_name and table_name and name_col):
-        dbconnect = MySQLdb.connect(host, login, password, db_name, port)
-        cur = dbconnect.cursor()
-        cur2 = dbconnect.cursor()
-        try:
-            cur.execute(
-                f"""CREATE TABLE IF NOT EXISTS {table_name} (id INT(12) auto_increment not null primary key, {name_col})""")
-            form.label_7.setText(
-                f"<font color=green>Таблица <font color=blue>{table_name}</font> в базе данных создана!!!</font>")
-        except:
-            dbconnect.rollback()
+        if (mysql_host and mysql_login and mysql_password and mysql_db_name):
+            dbcon = MySQLdb.connect(mysql_host, mysql_login, mysql_password, mysql_db_name, mysql_port)
+            cur = dbcon.cursor()
+            cur2 = dbcon.cursor()
+            try:
+                cur.execute(f"""CREATE TABLE IF NOT EXISTS {mysql_table_name} (id INT(12) auto_increment not null primary key, {mysql_name_col})""")
+                form.label_7.setText(f"<font color=green>Таблица <font color=blue>{mysql_table_name}</font> в базе данных создана!!!</font>")
+            except Exception as e:
+                form.label_7.setText("<font color=red>Соединение c MySql-сервером НЕ установленно!</font>")
+                print(f"""Ошибка: {e}""")
+                dbcon.rollback()
 
-        for body in range(len(ef2)):
-            rt = tuple(ef2.iloc[body].to_list())
-            print(rt)
-            cur2.execute(f"""INSERT INTO {table_name} ({name_col_insert}) VALUES {rt}""")
+            for body in range(len(mysql_ef2)):
+                mysql_rt = tuple(mysql_ef2.iloc[body].to_list())
+                print(mysql_rt)
+                cur2.execute(f"""INSERT INTO {mysql_table_name} ({mysql_name_col_insert}) VALUES {mysql_rt}""")
 
-        dbconnect.commit()
-        dbconnect.close()
+            dbcon.commit()
+            dbcon.close()
+        else:
+            form.label_7.setText("<font color=red>Ошибка заполнения!!!</font>")
     else:
-        form.label_7.setText("<font color=red>Ошибка заполнения!!!</font>")
+        print("Переменные 'server' или 'dbconnect' не найдены!")
+        form.label_7.setText("<font color=red>Нет подключения к SSH и/или MySql серверу!</font>")
 
 def on_click_windows():
     print("Закрыть Окно")
@@ -182,6 +186,7 @@ form.pushButton.clicked.connect(on_click_windows)
 form.pushButton_2.clicked.connect(on_click_testbd)
 form.pushButton_3.clicked.connect(on_click_excfile)
 form.pushButton_4.clicked.connect(on_click_create)
+form.pushButton_5.clicked.connect(on_click_testssh)
 
 app.exec_()
 
